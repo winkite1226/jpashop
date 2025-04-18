2c06e3d0ce7e8ca9e5b2721e29da105bafaa9f41
winki <winkite1226@gmail.com>
2025-04-18 15:34:47 +0900

    [Test]: 모든 상품 조회 기능 테스트

--- src/test/java/jpaproject/jpashop/service/ItemServiceTest.java
+++ src/test/java/jpaproject/jpashop/service/ItemServiceTest.java
@@ -2,11 +2,14 @@ package jpaproject.jpashop.service;
 
 import jpaproject.jpashop.domain.item.Album;
 import jpaproject.jpashop.domain.item.Item;
+import jpaproject.jpashop.domain.item.Movie;
 import jpaproject.jpashop.repository.ItemRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,4 +38,28 @@ class ItemServiceTest {
         assertEquals(album, itemRepository.findOne(saveId));
     }
 
+    @Test
+    public void findItems_test() throws Exception {
+        //given
+        Album album = new Album();
+        album.setArtist("Taylor Swift");
+        album.setName("anti hero DVD");
+        album.setPrice(15000);
+        album.setStockQuantity(50);
+
+        Movie movie = new Movie();
+        movie.setDirector("kim");
+        movie.setActor("lee");
+        movie.setName("black out");
+        movie.setPrice(20000);
+        movie.setStockQuantity(100);
+
+        //when
+        itemService.saveItem(album);
+        itemService.saveItem(movie);
+        List<Item> items = itemService.findItems();
+
+        //then
+        assertEquals(2, items.size());
+    }
 }
