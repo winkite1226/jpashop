cd5671b6c4e9a06483180d648cbd7588a8376c95
winki <winkite1226@gmail.com>
2025-04-18 12:00:04 +0900

    [Refactor]: 필드 주입을 생성자 주입으로 변경

--- src/main/java/jpaproject/jpashop/service/MemberService.java
+++ src/main/java/jpaproject/jpashop/service/MemberService.java
@@ -11,8 +11,11 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class MemberService {
 
-    @Autowired
-    MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     //회원가입
     @Transactional //변경
