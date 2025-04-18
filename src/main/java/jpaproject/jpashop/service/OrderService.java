4c2151c04079fbaaa5750db9c63f0edc242fd14c
winki <winkite1226@gmail.com>
2025-04-18 18:53:00 +0900

    [Feat]: 주문 생성 service 기능 추가

---
+++ src/main/java/jpaproject/jpashop/service/OrderService.java
@@ -0,0 +1,44 @@
+package jpaproject.jpashop.service;
+
+import jpaproject.jpashop.domain.*;
+import jpaproject.jpashop.domain.item.Item;
+import jpaproject.jpashop.repository.ItemRepository;
+import jpaproject.jpashop.repository.MemberRepository;
+import jpaproject.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+    private final OrderRepository orderRepository;
+
+    //주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+}
