ebd5724d6c8a61a61c6aadaf2767348aba01120f
winki <winkite1226@gmail.com>
2025-04-18 13:57:52 +0900

    [Test]: 회원가입 기능 테스트

---
+++ src/test/java/jpaproject/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,34 @@
+package jpaproject.jpashop.service;
+
+import jpaproject.jpashop.domain.Member;
+import jpaproject.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void join_test() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+}
