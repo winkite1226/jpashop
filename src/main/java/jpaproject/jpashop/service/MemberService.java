1d42ed071fb0d7bda9f5b3a5ce2246785d608efc
winki <winkite1226@gmail.com>
2025-04-18 11:54:45 +0900

    [Feat]: 회원가입 시 중복회원 검증 기능 추가

--- src/main/java/jpaproject/jpashop/service/MemberService.java
+++ src/main/java/jpaproject/jpashop/service/MemberService.java
@@ -5,6 +5,7 @@ import jpaproject.jpashop.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,7 +17,15 @@ public class MemberService {
     //회원가입
     @Transactional //변경
     public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 }
