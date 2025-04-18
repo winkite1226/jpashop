ad96886ae0c79f424a75ded890b71b2601b7f42a
winki <winkite1226@gmail.com>
2025-04-18 11:56:26 +0900

    [Feat]: 전체 회원, 특정 회원 조회 service 기능 추가

--- src/main/java/jpaproject/jpashop/service/MemberService.java
+++ src/main/java/jpaproject/jpashop/service/MemberService.java
@@ -28,4 +28,14 @@ public class MemberService {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
+
+    //전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    //특정 회원 조회
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
