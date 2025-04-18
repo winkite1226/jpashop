d2b486077badb2bb3317fb4a7b31ba0a6385b611
winki <winkite1226@gmail.com>
2025-04-18 15:05:43 +0900

    [Feat]: 상품 등록 기능 추가

---
+++ src/main/java/jpaproject/jpashop/repository/ItemRepository.java
@@ -0,0 +1,21 @@
+package jpaproject.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpaproject.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { //신규 상품
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+}
