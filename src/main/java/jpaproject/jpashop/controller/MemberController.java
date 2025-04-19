3455d41bd77849561fa3598b2d4428ce6ae1e822
winki <winkite1226@gmail.com>
2025-04-19 15:12:40 +0900

    [!BREAKING CHANGE]: 회원 생성 post api 추가

--- src/main/java/jpaproject/jpashop/controller/MemberController.java
+++ src/main/java/jpaproject/jpashop/controller/MemberController.java
@@ -1,10 +1,15 @@
 package jpaproject.jpashop.controller;
 
+import jakarta.validation.Valid;
+import jpaproject.jpashop.domain.Address;
+import jpaproject.jpashop.domain.Member;
 import jpaproject.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +21,21 @@ public class MemberController {
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
+    }
+
+    @PostMapping(value="/members/new")
+    public String create(@Valid MemberForm memberForm, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
