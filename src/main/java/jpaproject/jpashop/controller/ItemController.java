ba2959192deb264ad156d00439baab61a136955d
winki <winkite1226@gmail.com>
2025-04-20 15:53:04 +0900

    [!BREAKING CHANGE]: 상품 등록 화면 호출 get api 추가

---
+++ src/main/java/jpaproject/jpashop/controller/ItemController.java
@@ -0,0 +1,19 @@
+package jpaproject.jpashop.controller;
+
+import jpaproject.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+}
