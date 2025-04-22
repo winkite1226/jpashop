d86ec220468b1076f132710ef0b236b67d5bc516
winki <winkite1226@gmail.com>
2025-04-22 13:13:58 +0900

    [!BREAKING CHANGE]: 상품 주문 post api 추가

--- src/main/java/jpaproject/jpashop/controller/OrderController.java
+++ src/main/java/jpaproject/jpashop/controller/OrderController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,5 +31,11 @@ public class OrderController {
         model.addAttribute("items", items);
 
         return "order/orderForm";
+    }
+
+    @PostMapping(value = "/order")
+    public String order(@RequestParam("memberId") Long memberId, @RequestParam("itemId") Long itemId, @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
     }
 }
