76f521e08fd1e80e0f6590fa0af5aece7cbd8c6b
winki <winkite1226@gmail.com>
2025-04-22 13:11:06 +0900

    [!BREAKING CHANGE]: 상품 주문 화면 호출 get api 추가

---
+++ src/main/java/jpaproject/jpashop/controller/OrderController.java
@@ -0,0 +1,33 @@
+package jpaproject.jpashop.controller;
+
+import jpaproject.jpashop.domain.Member;
+import jpaproject.jpashop.domain.item.Item;
+import jpaproject.jpashop.service.ItemService;
+import jpaproject.jpashop.service.MemberService;
+import jpaproject.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping(value = "/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+}
