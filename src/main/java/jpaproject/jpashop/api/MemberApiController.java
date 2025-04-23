060d38b807d9fb683f0cac68bb19ce346b1667ad
winki <winkite1226@gmail.com>
2025-04-23 14:37:50 +0900

    [Feat]: 회원 등록 api 추가

---
+++ src/main/java/jpaproject/jpashop/api/MemberApiController.java
@@ -0,0 +1,32 @@
+package jpaproject.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpaproject.jpashop.domain.Member;
+import jpaproject.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/members")
+    public CreateMemberResponse saveMember(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    public static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
