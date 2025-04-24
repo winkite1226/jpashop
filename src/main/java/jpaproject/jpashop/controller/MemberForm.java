a5674c4e4402f9b58cdfd90fa9dae886a63fe15c
winki <winkite1226@gmail.com>
2025-04-24 16:24:17 +0900

    [!BREAKING CHANGE]: 회원 가입 정보 입력 form 객체 loginId, password 속성 추가

--- src/main/java/jpaproject/jpashop/controller/MemberForm.java
+++ src/main/java/jpaproject/jpashop/controller/MemberForm.java
@@ -7,7 +7,12 @@ import jakarta.validation.constraints.NotEmpty;
 
 @Getter @Setter
 public class MemberForm {
-
+    
+    @NotEmpty(message = "아이디는 필수입니다.")
+    private String loginId;
+    @NotEmpty(message = "비밀번호는 필수입니다.")
+    private String password;
+    
     @NotEmpty(message = "회원 이름은 필수입니다.")
     private String name;
     private String city;
