600206a8475e9f571e6e29493c024a807264a370
winki <winkite1226@gmail.com>
2025-04-24 16:25:42 +0900

    [Fix]: 특정 정보 회원 찾기 name에서 loginId로 변경

--- src/main/java/jpaproject/jpashop/repository/MemberRepository.java
+++ src/main/java/jpaproject/jpashop/repository/MemberRepository.java
@@ -27,9 +27,9 @@ public class MemberRepository {
                 .getResultList();
     }
 
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
+    public List<Member> findByLoginId(String loginId) {
+        return em.createQuery("select m from Member m where m.loginId = :loginId", Member.class)
+                .setParameter("loginId", loginId)
                 .getResultList();
     }
 
--- src/main/java/jpaproject/jpashop/service/MemberService.java
+++ src/main/java/jpaproject/jpashop/service/MemberService.java
@@ -24,7 +24,7 @@ public class MemberService {
     }
 
     private void validateDuplicateMember(Member member) {
-        List<Member> findMembers = memberRepository.findByName(member.getName());
+        List<Member> findMembers = memberRepository.findByLoginId(member.getLoginId());
         if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
